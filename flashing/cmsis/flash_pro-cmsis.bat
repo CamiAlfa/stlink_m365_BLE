063a2b5ce32d0bd4cce3c381009ac3dad7ea6a72
CamiAlfa <31709457+CamiAlfa@users.noreply.github.com>
2021-05-25 18:46:07 +0200

    Update V9

---
+++ flashing/cmsis/flash_pro-cmsis.bat
@@ -0,0 +1,9 @@
+@echo "you have 10 seconds to connect wires"
+@timeout 10
+@openocd.exe -f ../scripts/interface/cmsis-dap.cfg -f ../scripts/target/nrf51.cfg -c "init" -c "reset halt" -c "nrf51 mass_erase" -c "program pro_074V9.hex verify" -c "reset run" -c "exit"
+@echo "If board doesnt flash press any key to try again at lower speed"
+@pause
+@echo "now we are going to try slow mode, you have 10 seconds to connect wires"
+@timeout 10
+@openocd.exe -f ../scripts/interface/cmsis-dap.cfg -f ../scripts/target/nrf51_100k.cfg -c "init" -c "reset halt" -c "nrf51 mass_erase" -c "program pro_074V9.hex verify" -c "reset run" -c "exit"
+@pause
